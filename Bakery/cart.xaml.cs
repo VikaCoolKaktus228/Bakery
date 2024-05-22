d59413913efe1f136019c317df08c62eb3352b57
delovayakolbasa <puv2912@gmail.com>
2024-05-22 23:01:18 +0300

    немножко переделан qrcode

--- Bakery/cart.xaml.cs
+++ Bakery/cart.xaml.cs
@@ -13,6 +13,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Image = iTextSharp.text.Image;
+using System.Windows.Markup;
+using System.IO;
+using System.Xml.Linq;
+using Paragraph = iTextSharp.text.Paragraph;
 
 namespace Bakery
 {
@@ -22,10 +29,11 @@ namespace Bakery
     /// 
     public partial class cart : Page
     {
+        int idusercart = Convert.ToInt32(App.Current.Properties["Id"].ToString());
 
         public cart()
         {
-            int idusercart = Convert.ToInt32(App.Current.Properties["Id"].ToString());
+           
             InitializeComponent();
 
             var orderobj = Entities.GetContext().Order
@@ -39,6 +47,60 @@ namespace Bakery
             cartbakery.ItemsSource = goodsInCart;
         }
 
+        private void CreatePDF()
+        {
+            Document doc = new Document();
+
+            try
+            {
+                PdfWriter.GetInstance(doc, new FileStream("..\\..\\output.pdf", FileMode.Create));
+
+                doc.Open();
+                BaseFont basefont = BaseFont.CreateFont("C:\\Windows\\Fonts\\Arial.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+
+                Font font = new Font(basefont, 12);
+                Font font1 = new Font(basefont, 25, 3, BaseColor.BLUE);
+                Paragraph paragraph1 = new Paragraph("список товаров", font1);
+                doc.Add(paragraph1);
+                int sum = 0;
+
+                var orderobj = Entities.GetContext().Order
+                               .Where(x => x.IdUser == idusercart)
+                               .ToList();
+
+                foreach (var item in orderobj)
+                {
+                    if (item is Order)
+                    {
+                        Order data = (Order)item;
+                        //Image img = Image.GetInstance("C:\\Users\\User\\Pictures\\bakeryhome\\Bakery\\" + data.GoodsBakery.CurrentPhoto);
+                        //img.ScaleAbsolute(100f, 100f);
+                        //doc.Add(img);
+                        doc.Add(new Paragraph("Haзвaние: " + data.GoodsBakery.NameGoods, font));
+                        doc.Add(new Paragraph("Oпиcaние: " + data.GoodsBakery.Description, font));
+                        doc.Add(new Paragraph("производитель " + data.GoodsBakery.Provider1.Provider1, font));
+                        doc.Add(new Paragraph("Cтоимость: " + data.GoodsBakery.Price.ToString() + "py6.", font));
+                        sum += (int)data.GoodsBakery.Price;
+                    }
+                }
+                Paragraph paragraph = new Paragraph("Cymma = " + sum.ToString(), font);
+                paragraph.Alignment = Element.ALIGN_RIGHT;
+                doc.Add(paragraph);
+            }
+            catch (DocumentException de)
+            {
+                Console.WriteLine(de.Message);
+            }
+            catch (IOException ioe)
+            {
+                Console.Error.WriteLine(ioe.Message);
+            }
+            finally
+            {
+                doc.Close();
+            }
+        }
+
         private void removecart_Click(object sender, RoutedEventArgs e)
         {
 
@@ -50,6 +112,8 @@ namespace Bakery
         }
         private void orderbutton_Click(object sender, RoutedEventArgs e)
         {
+            CreatePDF();
+
             AppFrame.BakeryFrame.Navigate(new OrderForm());
 
         }
